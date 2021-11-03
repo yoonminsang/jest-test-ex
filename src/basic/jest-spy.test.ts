9bfad6b6f050793ce7c0935819d1f22a27929b40
yoonminsang <apdlyooapdl@naver.com>
2021-11-03 20:04:25 +0900

    feat: jest.spy 테스트

---
+++ src/basic/jest-spy.test.ts
@@ -0,0 +1,13 @@
+test('fn test', () => {
+  const calculator = {
+    add: (a: number, b: number) => a + b,
+  };
+
+  const spyFn = jest.spyOn(calculator, 'add');
+
+  const result = calculator.add(2, 3);
+
+  expect(spyFn).toBeCalledTimes(1);
+  expect(spyFn).toBeCalledWith(2, 3);
+  expect(result).toBe(5);
+});
